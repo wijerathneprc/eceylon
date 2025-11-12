5877ac6861416fba7b95a224a9a9138b4fea6885
Rishan Wijerathne <wijerathneprc92@gmail.com>
2025-11-12 16:30:23 +0530

    fix the error in scroll button feature

--- script.js
+++ script.js
@@ -15,35 +15,31 @@ function rightScroll(container){
 
 document.getElementById('left-btn-customer').addEventListener('click', ()=>{
     const container = document.getElementsByClassName('customer-grid')[0];
-    leftScroll(container)
-})
+    leftScroll(container);
+});
 
 document.getElementById('right-btn-customer').addEventListener('click', ()=>{
-    const container = document.getElementsByClassName('customer-grid')[0];
-    rightScroll(container)
-})
+    let container = document.getElementsByClassName('customer-grid')[0];
+    rightScroll(container);
+});
 
 
 document.getElementById('left-btn-new').addEventListener('click', ()=>{
     const container = document.getElementsByClassName('product-grid')[1];
-    leftScroll(container)
-
-})
+    leftScroll(container);
+});
 
 document.getElementById('right-btn-new').addEventListener('click', ()=>{
     const container = document.getElementsByClassName('product-grid')[1];
-    rightScroll(container)
-
-})
+    rightScroll(container);
+});
 
 document.getElementById('left-btn-discount').addEventListener('click', ()=>{
     const container = document.getElementsByClassName('product-grid')[0];
-    leftScroll(container)
-
-})
+    leftScroll(container);
+});
 
 document.getElementById('right-btn-discount').addEventListener('click', ()=>{
     const container = document.getElementsByClassName('product-grid')[0];
-    rightScroll(container)
-
-})
+    rightScroll(container);
+});
